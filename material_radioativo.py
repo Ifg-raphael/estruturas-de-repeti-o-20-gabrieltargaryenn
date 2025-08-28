8bb03fca1df889ceb4cfbc3d46db52afb1533c54
gabrieltargaryenn <gabrielalvestargaryen@gmail.com>
2025-08-28 20:11:21 -0300

    atividade finalizada

--- material_radioativo.py
+++ material_radioativo.py
@@ -1,1 +1,15 @@
 # Sua solução aqui
+
+massa = float(input())
+
+tempo_total = 0  # tempo em segundos
+
+while massa >= 0.5:
+    massa /= 2
+    tempo_total += 50
+
+horas = tempo_total // 3600
+minutos = (tempo_total % 3600) // 60
+segundos = tempo_total % 60
+
+print(f"{int(horas)}h {int(minutos)}m {int(segundos)}s")
